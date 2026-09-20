1bfe648150416713a104fcd6fd4b30b5a0803c2d
Howland Mai <mai_haoliang@163.com>
2026-09-20 16:09:41 +0800

    feat: add new algorithm bsearch simple

--- include/algo.hpp
+++ include/algo.hpp
@@ -1,5 +1,12 @@
-template <typename T> void myswap(T &a, T &b) {
+#include <vector>
+namespace algo {
+
+template <typename T> void MySwap(T &a, T &b) {
   T tmp = a;
   a = b;
   b = tmp;
 }
+
+int BinarySearch(const std::vector<int> &nums, int target);
+
+} // namespace algo
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "algo.hpp"
 #include <format>
 #include <iostream>
+using namespace algo;
 int main(int argc, char **argv) {
-  std::cout << "hello world!" << std::endl;
   int a = 10, b = 2;
-  myswap(a, b);
+  MySwap(a, b);
   std::cout << std::format("a = {}, b = {}", a, b) << std::endl;
   return 0;
 }
